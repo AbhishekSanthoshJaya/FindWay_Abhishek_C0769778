c03450ef33e53a6839efb7751fd2cdff17194aa1
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-06-10 17:14:51 -0400

    Fomatting, removing old markers

--- FindWay_Abhishek_C0769778/ViewController.swift
+++ FindWay_Abhishek_C0769778/ViewController.swift
@@ -12,40 +12,57 @@ import MapKit
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    var locationManager:CLLocationManager = CLLocationManager()
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         mapView.delegate = self
+        locationManager.delegate = self
+        
+        //Permission and finding inital location
         locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = kCLDistanceFilterNone
         locationManager.startUpdatingLocation()
         
+        
+        //Zooming to user location
+        if let userLocation = locationManager.location?.coordinate
+        {
+            let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 200, longitudinalMeters: 200)
+            mapView.setRegion(viewRegion, animated: false)
+        }
         mapView.showsUserLocation = true
-        //Zoom to user location
-        if let userLocation = locationManager.location?.coordinate {
-        let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 200, longitudinalMeters: 200)
-        mapView.setRegion(viewRegion, animated: false)
-    }
+        
+        //Gesture to add marker
         let longTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
-    mapView.addGestureRecognizer(longTapGesture)
+        mapView.addGestureRecognizer(longTapGesture)
         
   }
-    @objc func longTap(sender: UIGestureRecognizer){
-    print("long tap")
-    if sender.state == .began {
-        let locationInView = sender.location(in: mapView)
-        let locationOnMap = mapView.convert(locationInView, toCoordinateFrom: mapView)
-        addAnnotation(location: locationOnMap)
-    }
-}
+        @objc func longTap(sender: UIGestureRecognizer)
+        {
+        // Annotation at long press location
+            if sender.state == .began
+            {
+                let locationInView = sender.location(in: mapView)
+                let locationOnMap = mapView.convert(locationInView, toCoordinateFrom: mapView)
+                addAnnotation(location: locationOnMap)
+            }
+        }
 
-func addAnnotation(location: CLLocationCoordinate2D){
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "Destination"
-        annotation.subtitle = "Destination"
-        self.mapView.addAnnotation(annotation)
-  }
+        func addAnnotation(location: CLLocationCoordinate2D)
+        {
+            //Removing previous annotations
+            let oldAnnotations = self.mapView.annotations
+            self.mapView.removeAnnotations(oldAnnotations)
+            
+            //Adding new annotation
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            annotation.title = "Destination"
+            annotation.subtitle = "Destination"
+            self.mapView.addAnnotation(annotation)
+        }
 }
 
